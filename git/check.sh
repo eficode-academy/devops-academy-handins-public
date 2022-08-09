f73a1a0f2b60d0d6fe3a50a9e72516ef58e6663e
Sofus Albertsen <sofusalbertsen@gmail.com>
2022-08-09 08:58:49 +0200

    EDIT check.sh all pull before use release tag

--- git/check.sh
+++ git/check.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 EXERCISE_PATH=$(dirname $(realpath "$0"))
+#make sure that you are working with the latest version of the checker
+docker pull eficodeacademy/devops-academy-checker:release
 
 docker run \
     --rm \
--- kubernetes/check.sh
+++ kubernetes/check.sh
@@ -2,11 +2,14 @@
 
 EXERCISE_PATH=$(dirname $(realpath "$0"))
 
+#make sure that you are working with the latest version of the checker
+docker pull eficodeacademy/devops-academy-checker:release
+
 docker run \
   --rm \
   -it \
   --network host \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$EXERCISE_PATH":/exercise \
-  eficodeacademy/devops-academy-checker:release-3 \
+  eficodeacademy/devops-academy-checker:release \
   kubernetes $*
